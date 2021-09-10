54e9cd5f9fe2991f1d4308dc89e0dcfdbdb82f69
tuhindasv0 <tuhindasv0@gmail.com>
2021-09-10 17:18:22 +0530

    trying basepath

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   assetPrefix: 'testprefix',
-  rewrites() {
-    return [
-      { source: '/testprefix/_next/:path*', destination: '/_next/:path*' }
-    ]
-  },
-  future:{webpack5:false}
+  basePath: 'testprefix'
+ 
 }
