eb22377be9925a7529dfe2d065d5a10313d10979
tuhindasv0 <tuhindasv0@gmail.com>
2021-09-10 16:56:13 +0530

    adding rewrite

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   reactStrictMode: true,
   assetPrefix: 'testprefix',
+  rewrites() {
+    return [
+      { source: '/testprefix/_next/:path*', destination: '/_next/:path*' }
+    ]
+  },
   future:{webpack5:false}
 }
